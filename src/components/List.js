583ed4a4b519e686545b99e362e21f96ee6d2119
Fox Scarlett <foxontheone@gmail.com>
2023-11-14 21:26:59 -0800

    create list item component

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/List.module.css'
 
+import ListItem from '@/components/ListItem'
+
 export default function List() {
   return (
     <div className={styles.list_wrapper}>
       <ul>
-        <li>List Item</li>
-        <li>List Item</li>
-        <li>List Item</li>
-        <li>List Item</li>
-        <li>List Item</li>
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
       </ul>
     </div>
   )
