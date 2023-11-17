eb2053b00308c1a6e6b0b41f1f2042b55d72c7fa
Fox Scarlett <foxontheone@gmail.com>
2023-11-16 22:36:16 -0800

    add param stubs to list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,6 +7,12 @@ export default function ListItem() {
     <li className={styles.list_item_wrapper}>
       <h2>List Item Title</h2>
       <p>List Item Details</p>
+      <div className={styles.list_item_status_wrapper}>
+        <span className={styles.list_item_status}>Level</span>
+        <span className={styles.list_item_status}>Status</span>
+        <span className={styles.list_item_status}>Project</span>
+        <span className={styles.list_item_status}>Due</span>
+      </div>
     </li>
   )
 }
