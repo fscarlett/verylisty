646fcee19229313a688a8810c416c9efa5ac86ac
Fox Scarlett <foxontheone@gmail.com>
2023-11-21 21:05:12 -0800

    note to self styles

--- src/components/NoteToSelf.js
+++ src/components/NoteToSelf.js
@@ -4,9 +4,9 @@ import styles from '@/styles/List.module.css'
 
 export default function NoteToSelf() {
   return (
-    <div className={styles.left_side_content_wrapper}>
+    <div className={styles.note_to_self_wrapper}>
       <h2>Note to self</h2>
-      <div className={styles.left_side_content}>
+      <div className={styles.note_to_self_content}>
         left side content Lorem ipsum, dolor sit amet consectetur adipisicing
         elit. Ipsam magni reiciendis, sint eum quas ea dolorum sunt aspernatur
         nesciunt laboriosam possimus. Dolores accusamus minima voluptatibus
